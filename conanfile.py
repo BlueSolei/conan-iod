c6d0bda074dd09f176c9343b87b798b99d6ec0f6
Shaul Fridman <sfridman@synamedia.com>
2019-06-10 18:16:13 +0300

    (+) Working IOD test

--- conanfile.py
+++ conanfile.py
@@ -6,22 +6,19 @@ from conans import ConanFile, tools
 class IodConan(ConanFile):
     name = "iod"
     version = "0.1"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Iod here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    license = "MIT"
+    author = "Matthieu Garrigues <matthieu.garrigues@gmail.com>"
+    url = "https://github.com/BlueSolei/conan-iod"
+    description = "Meta programming utilities for C++14"
+    topics = ("c++", "c-plus-plus", "meta-programming",
+              "serialization", "deserialization")
     no_copy_source = True
+    scm = {
+        "type": "git",
+        "url": "https://github.com/matt-42/iod",
+        "revision": "master"
+    }
     # No settings/options are necessary, this is header only
 
-    def source(self):
-        '''retrieval of the source code here. Remember you can also put the code
-        in the folder and use exports instead of retrieving it with this
-        source() method
-        '''
-        # self.run("git clone ...") or
-        # tools.download("url", "file.zip")
-        # tools.unzip("file.zip" )
-
     def package(self):
-        self.copy("*.h", "include")
+        self.copy("iod/*.*", "include")
---
+++ test_package/Main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#include "JsonTest.h"
+
+int main() { return JsonTest(); }
